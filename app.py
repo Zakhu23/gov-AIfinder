4bd7906f1beaaad47162926ec44d0bded2e9b600
Zakhu23 <zakhwan5168@gmail.com>
2026-05-03 00:38:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "STEP 1 WORKING 🚀"
+    return render_template("index.html")
