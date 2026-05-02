cd376647e796e5dbd298c4ab3423eed657782eca
Zakhu23 <zakhwan5168@gmail.com>
2026-05-03 00:56:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    results = []
+
+    if request.method == "POST":
+        age = int(request.form["age"])
+        income = int(request.form["income"])
+        education = request.form["education"]
+        occupation = request.form["occupation"]
+        state = request.form["state"]
+
+        # TEMP LOGIC (we will upgrade later)
+        if occupation == "Student":
+            results.append("Scholarship Scheme")
+
+        if occupation == "Farmer":
+            results.append("PM Kisan")
+
+        if education == "Graduate":
+            results.append("UPSC / SSC Exams")
+
+        if income < 300000:
+            results.append("Low Income Scheme")
+
+    return render_template("index.html", results=results)
